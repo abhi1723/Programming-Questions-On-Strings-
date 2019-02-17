49b9bc39ee7d9e7c6d72bbdad088411d635cc9d9
Abhishek Kumar <abhishekisme1996@hotmail.com>
2019-02-18 00:45:13 +0530

    Print "lowercase" if user enters a character between a-z , Print "UPPERCASE" is character lies between A-Z and print "Invalid" for all other characters like $,.^# etc.

---
+++ LOWER_UPPER/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letter;
+    cin>>letter;
+    int ascii_form_of_letter=(int)letter;
+    if(ascii_form_of_letter>64 &&ascii_form_of_letter<91)
+        cout<<"UPPERCASE";
+    else if(ascii_form_of_letter>96 &&ascii_form_of_letter<123)
+        cout<<"LOWERCASE";
+    else
+        cout<<"Invalid";
+    return 0;
+}
